1d1a51dc65e8d080626d9069b2b2817d9fc37a70
Abhilash Purohit <abhilash.purohit@gmail.com>
2025-02-11 17:22:44 +0530

    Slugs and articles v4

--- app/api/articles/[slug]/route.ts
+++ app/api/articles/[slug]/route.ts
@@ -2,11 +2,11 @@ import {NextRequest, NextResponse} from 'next/server';
 import {db} from '../../../../firebase';
 
 interface Props {
-    params: {slug: string};
+    params: Promise<{ slug: string }>;
 }
 
-export async function GET(request: NextRequest, context: {params: {slug: string}}) {
-  const { slug } = context.params;
+export async function GET(request: NextRequest, {params} : Props) {
+  const { slug } = await params;
   const response = await db.collection('articles').where("slug", "==", slug).get();
   const article = response.docs[0].data();
   return NextResponse.json(article);
