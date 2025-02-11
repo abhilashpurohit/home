e4fb7bad3025a9d611924a05367957576a20de62
Abhilash Purohit <abhilash.purohit@gmail.com>
2025-02-11 17:46:28 +0530

    Slugs and articles v6

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { db } from '../../../firebase';
 
 export async function GET(request: NextRequest) {
-  const response = await db.collection('articles').get();
-  const articles = response.docs.map(doc => doc.data());
-  return NextResponse.json(articles);
+    const response = await db.collection('articles').get();
+    const articles = response.docs.map(doc => doc.data());
+    return NextResponse.json(articles);
 }
--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { NextResponse, NextRequest } from "next/server";
 import { API_URL } from "@/config/api";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface Article {
     slug: string;
@@ -7,9 +10,21 @@ interface Article {
     content: string;
 }
 
-export default async function Article(){
-    const res = await fetch(`${API_URL}/articles`);
-    const articles: Article[] = await res.json();
+export default function Article(){
+
+    const [articles, setArticles] = useState<Article[]>([]);
+
+    useEffect(() => {
+        async function getArticle() {
+            const response = await fetch(`${API_URL}/articles`);
+            if(!response.ok){
+                return NextResponse.error();
+            }
+            const articles: Article[] = await response.json();
+            setArticles(articles);
+        }
+        getArticle();
+    },[])
     return (
         <div>
             {articles.map((article) => (
